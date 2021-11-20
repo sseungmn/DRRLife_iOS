762b88f99bdd398d63c89da21213d8f033880249
sseungmn <severck12@naver.com>
2021-11-20 18:33:39 +0900

    Implement extension fot using apikey

---
+++ DRRLife/DRRLife++Bundle.swift
@@ -0,0 +1,30 @@
+//
+//  DRRLife++Bundle.swift
+//  DRRLife
+//
+//  Created by OHSEUNGMIN on 2021/11/20.
+//
+
+import Foundation
+
+extension Bundle {
+    var NMFClientId: String {
+        return getKey(of: "NFMClientId")
+    }
+    var NaverMap: String {
+        return getKey(of: "NaverMap")
+    }
+    var KakaoLocal: String {
+        return getKey(of: "KakaoLocal")
+    }
+    var Openroute: String {
+        return getKey(of: "Openroute")
+    }
+    
+    private func getKey(of name: String)-> String {
+        guard let file = path(forResource: "APIKeys", ofType: "plist") else { fatalError("APIKeys.plist 파일이 존재하지 않습니다.") }
+        guard let resource = NSDictionary(contentsOfFile: file) else { fatalError("APIKeys.plist 파일의 형식이 알맞지 않습니다.") }
+        guard let key = resource[name] as? String else { fatalError("해당 API Key가 존재하지 않습니다.")}
+        return key
+    }
+}
