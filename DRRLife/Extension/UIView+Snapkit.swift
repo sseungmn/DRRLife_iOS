7cd29d18a3b601c6663901a9e48bfead2c9a7b99
sseungmn <severck12@naver.com>
2021-11-22 16:28:12 +0900

    Implement extension for easy access to safeArea

---
+++ DRRLife/Extension/UIView+Snapkit.swift
@@ -0,0 +1,15 @@
+//
+//  UIView+Snapkit.swift
+//  DRRLife
+//
+//  Created by OHSEUNGMIN on 2021/11/22.
+//
+
+import Foundation
+import SnapKit
+
+extension UIView {
+    var safeArea: ConstraintLayoutGuideDSL {
+        return safeAreaLayoutGuide.snp
+    }
+}
